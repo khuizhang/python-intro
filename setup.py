3afd573edfd35d3e769322f48183177624e0874b
Kevin Zhang <khuizhang@gmail.com>
2020-03-18 09:43:48 -0700

    update vscode settings and pipenv files

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DESCRIPTION = 'parse offline storagegrid logs'
 URL = 'https://github.com/khuizhang/storagegrid'
 EMAIL = 'kevin.zhang@netapp.com'
 AUTHOR = 'Kevin Zhang'
-REQUIRES_PYTHON = '>=3.6.0'
+REQUIRES_PYTHON = '>=3.7.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
@@ -120,7 +120,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
