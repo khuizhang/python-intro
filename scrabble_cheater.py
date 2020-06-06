66d9f2e948a2a185f769098848f1a5e8f098a4c4
Kevin Zhang <khuizhang@gmail.com>
2020-06-06 14:11:48 -0700

    changed storagegrid repo to python tutor contents

---
+++ scrabble_cheater.py
@@ -0,0 +1,40 @@
+import sys
+import .scrabble
+
+
+def valid_word(word, rack):
+    # make a copy of the rack for every new workd, so we can manipulate it
+    # with compromising the original rack.
+    avaialable_letters = rack[:]
+    for letter in word:
+        if letter not in avaialable_letters:
+            return False
+        avaialable_letters.remove(letter)
+    return True
+
+
+def compute_score(word):
+    # Calculate the Scrabble score for the word.
+    score = 0
+    for letter in word:
+        score = score + scrabble.scores[letter]
+    return score
+
+
+if len(sys.argv) < 2:
+    print("Usage: scrabble.py [RACK]")
+    exit(1)
+
+rack = list(sys.argv[1].lower())
+valid_words = []
+
+for word in scrabble.wordlist:
+    if valid_word(word, rack):
+        score = compute_score(word)
+        valid_words.append([score, word])
+
+valid_words.sort()
+for play in valid_words:
+    score = play[0]
+    word = play[1]
+    print(word + ": " + str(score))
